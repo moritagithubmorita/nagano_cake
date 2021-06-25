7d06d3195eee24a2d75de478f3616125c1636d49
EC2 Default User <ec2-user@ip-172-31-91-79.ec2.internal>
2021-06-25 14:52:21 +0000

    [Update]nagano_cake admin側css以外完成2?

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,7 +2,7 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
   end
-  
+
   def update
     order = Order.find(params[:id])
     order.update(order_update_params)
@@ -11,11 +11,12 @@ class Admin::OrdersController < ApplicationController
       order.order_items.all.each do |order_item|
         OrderItems.update_making_status(order_item, :wait)
       end
+    end
   end
-  
+
   def self.update_order_status(order)
     complete = true #全ての商品のステータスが「製作完了」かどうか
-    
+
     order.order_items.all.each do |order_item|
       #1個でも製作中なら製作ステータスを「製作中」にする
       if order_item.making?
@@ -27,16 +28,16 @@ class Admin::OrdersController < ApplicationController
         complete=false
       end
     end
-    
+
     #製作完了に変更
     if complete
       order.complete!
       order.save
     end
   end
-  
+
   private
-  
+
   def order_update_params
     params.require(:order).permit(:order_status)
   end
